a7f1749f015035a83a55f01af46383480203c839
Samuel <samuelfs96@gmail.com>
2024-01-17 02:04:16 -0400

    feat: create main template

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import './App.css'
+import "./App.css";
+import MatchBox from "./components/MatchBox";
 
 function App() {
-
   return (
-    <div className='bg-bgchampions w-screen h-screen bg-contain bg-center'>
-
+    <div className="bg-bgchampions w-screen h-screen bg-contain bg-center flex justify-center gap-[32rem] items-center">
+      <div className="flex flex-col gap-6">
+        <MatchBox />
+        <MatchBox />
+        <MatchBox />
+        <MatchBox />
+      </div>
+      <div className="flex flex-col gap-6">
+        <MatchBox reverse />
+        <MatchBox reverse />
+        <MatchBox reverse />
+        <MatchBox reverse />
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
