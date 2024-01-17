1baf0056d2491aec6e7d0aee9d76af5fbaa60eb8
Samuel <samuelfs96@gmail.com>
2024-01-16 23:10:55 -0400

    feat: add champions bg

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'bgchampions': "url('../src/assets/img/bgchampions.jpg')",
+      }
+    },
   },
   plugins: [],
 }
