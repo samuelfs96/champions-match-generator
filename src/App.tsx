e889665c4806e7f11358f230c473ff21cd699e55
Samuel <samuelfs96@gmail.com>
2024-01-21 19:35:45 -0400

    feat: style changes and clear component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
 import "./App.css";
 import MatchBox from "./components/MatchBox";
+import logo1 from "./assets/img/teams/rea.png";
+import logo2 from "./assets/img/teams/bar.png";
+import { Team } from "./types/team";
+import { useState } from "react";
+import { Instances } from "./types/instances";
+
+const teamA: Team = {
+  name: "Real madrid",
+  logo: logo1,
+  score: 4,
+};
+
+const teamB: Team = {
+  name: "Barcelona",
+  logo: logo2,
+  score: 2,
+};
 
 function App() {
+
+  const [instance, setInstance] = useState(Instances.ROUND16)
+
   return (
-    <div className="bg-bgchampions w-screen h-screen bg-contain bg-center flex justify-center gap-[32rem] items-center">
-      <div className="flex flex-col gap-6">
-        <MatchBox />
-        <MatchBox />
-        <MatchBox />
-        <MatchBox />
-      </div>
-      <div className="flex flex-col gap-6">
-        <MatchBox reverse />
-        <MatchBox reverse />
-        <MatchBox reverse />
-        <MatchBox reverse />
+    <div className="bg-bgchampions w-screen h-screen bg-contain bg-center flex flex-col items-center justify-center">
+      <h1 className=" text-white mb-2 uppercase font-mono font-bold flex flex-col">
+        <span className="text-stroke-3 text-center text-2xl">{instance}</span>
+        <span className="bg-white text-center text-black px-2">champions 2023-2024</span>
+      </h1>
+      <div className="flex justify-center gap-[32rem] items-center">
+        <div className="flex flex-col gap-6">
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+
+        </div>
+        <div className="flex flex-col gap-6">
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+          <MatchBox teamA={teamA} teamB={teamB} />
+        </div>
       </div>
     </div>
   );
--- src/components/MatchBox.tsx
+++ src/components/MatchBox.tsx
@@ -1,53 +1,44 @@
-import logo1 from "../assets/img/teams/bar.png";
-import logo2 from "../assets/img/teams/rea.png";
+import { Team } from "../types/team";
 
 interface MatchBoxProps {
-  reverse?: boolean;
+  teamA: Team;
+  teamB: Team;
 }
 
-export default function MatchBox({ reverse }: MatchBoxProps) {
+export default function MatchBox({ teamA, teamB }: MatchBoxProps) {
   return (
     <div
-      className={`flex justify-between items-center border-[#fe30fd80] border-[1px] px-8 py-4 gap-12 ${
-        reverse ? "" : "-"
-      }skew-x-12 [&>*]:${reverse ? "-" : ""}skew-x-12`}
+      className={`flex justify-between items-center border-[#fe30fd80] border-[1px] px-8 py-4 gap-12 -skew-x-12 [&>*]:skew-x-12`}
     >
       <div className="flex justify-center items-center">
         <div className="flex items-center flex-col gap-1">
-          <img className="w-[62px] h-[62px]" src={logo1} alt="teamlogo1" />
+          <img className="w-[62px] h-[62px]" src={teamA.logo} alt="teamlogo1" />
           <h1 className="text-center text-white uppercase text-xs font-semibold">
-            Barcelona
+            {teamA.name}
           </h1>
         </div>
       </div>
       <div className="flex justify-center items-center">
         <div
-          className={`bg-[#1745fb] w-10 h-12 flex justify-center items-center ${
-            reverse ? "" : "-"
-          }skew-x-12 [&>*]:${reverse ? "-" : ""}skew-x-12`}
+          className={`bg-[#fe30fd80] w-10 h-12 flex justify-center items-center -skew-x-12 [&>*]:skew-x-12`}
         >
-          <span className="text-white text-2xl font-mono font-bold">1</span>
+          <span className="text-white text-2xl font-mono font-bold">
+            {teamA.score}
+          </span>
         </div>
         <div
-          className={`bg-[#fe30fd80] w-10 h-12 flex justify-center items-center ${
-            reverse ? "" : "-"
-          }skew-x-12 [&>*]:${reverse ? "-" : ""}skew-x-12`}
+          className={`bg-white w-10 h-12 flex justify-center items-center -skew-x-12 [&>*]:skew-x-12`}
         >
-          <span className="text-white text-lg">vs</span>
-        </div>
-        <div
-          className={`bg-white w-10 h-12 flex justify-center items-center ${
-            reverse ? "" : "-"
-          }skew-x-12 [&>*]:${reverse ? "-" : ""}skew-x-12`}
-        >
-          <span className="text-[#010449] text-2xl font-mono font-bold">4</span>
+          <span className="text-[#81209b] text-2xl font-mono font-bold">
+            {teamB.score}
+          </span>
         </div>
       </div>
       <div className="flex justify-center items-center">
         <div className="flex items-center flex-col gap-1">
-          <img className="w-[62px] h-[62px]" src={logo2} alt="teamlogo2" />
+          <img className="w-[62px] h-[62px]" src={teamB.logo} alt="teamlogo2" />
           <h1 className="text-center text-white uppercase text-xs font-semibold">
-            Real madrid
+            {teamB.name}
           </h1>
         </div>
       </div>
