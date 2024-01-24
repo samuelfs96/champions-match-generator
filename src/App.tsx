0da62bf673d90a9404da5dcb4190a339ebf2aea3
Samuel <samuelfs96@gmail.com>
2024-01-23 22:52:16 -0400

    feat: fix responsive dimensions

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ function App() {
   } = useMatchup(teams);
 
   return (
-    <div className={`bg-bgchampions w-screen max-lg:w-auto h-screen ${instance === Instances.ROUND16 ? 'max-lg:h-auto max-lg:pt-32 max-lg:pb-28' : ''} bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center`}>
+    <div className={`bg-bgchampions w-screen max-lg:w-auto h-screen ${instance === Instances.ROUND16 ? 'max-lg:h-auto max-lg:pt-32 max-lg:pb-28' : ''} bg-no-repeat bg-cover max-lg:bg-auto max-lg:bg-repeat-y bg-center flex flex-col items-center justify-center`}>
       <div className="fixed top-0 left-0 flex w-full justify-between p-8 z-20 max-lg:bg-[#81209b80]">
       <div className="  bg-white -skew-x-12 [&>*]:skew-x-12 shadow-[5px_5px_0_0_rgba(254,48,253,0.5)]">
         <button
@@ -50,7 +50,7 @@ function App() {
       </h1>
       <div
         className={`flex justify-center mt-8 ${
-          instance === Instances.FINAL ? "" : "gap-[32rem] max-lg:gap-0"
+          instance === Instances.FINAL ? "" : "gap-[32rem] max-lg:gap-6"
         } items-center max-lg:flex-col`}
       >
         <div className="flex flex-col gap-6">
