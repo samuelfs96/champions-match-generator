687c2ab7b227ae8ae3122373ce034f2231afa2ef
Samuel <samuelfs96@gmail.com>
2024-01-24 01:11:53 -0400

    feat: add animations w framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import refresh_icon from "./assets/img/icons/refresh.png";
 import next_icon from "./assets/img/icons/next.png";
 import { Instances } from "./types/instances";
 import { matchkeys } from "./utils";
+import { motion } from "framer-motion";
 
 function App() {
   const {
@@ -20,34 +21,48 @@ function App() {
   } = useMatchup(teams);
 
   return (
-    <div className={`bg-bgchampions w-screen max-lg:w-auto h-screen ${instance === Instances.ROUND16 ? 'max-lg:h-auto max-lg:pt-32 max-lg:pb-28' : ''} bg-no-repeat bg-cover max-lg:bg-auto max-lg:bg-repeat-y bg-center flex flex-col items-center justify-center`}>
+    <div
+      className={`bg-bgchampions w-screen max-lg:w-auto h-screen ${
+        instance === Instances.ROUND16
+          ? "max-lg:h-auto max-lg:pt-32 max-lg:pb-28"
+          : ""
+      } bg-no-repeat bg-cover max-lg:bg-auto max-lg:bg-repeat-y bg-center flex flex-col items-center justify-center`}
+    >
       <div className="fixed top-0 left-0 flex w-full justify-between p-8 z-20 max-lg:bg-[#81209b80]">
-      <div className="  bg-white -skew-x-12 [&>*]:skew-x-12 shadow-[5px_5px_0_0_rgba(254,48,253,0.5)]">
-        <button
-          className="px-3 py-3 text-[#81209b] uppercase text-sm hover:[&>img]:rotate-90"
-          onClick={() => handleRefresh()}
-        >
-          <img
-            src={refresh_icon}
-            alt="refresh"
-            className="w-6 transition-all"
-          />
-        </button>
-      </div>
+        <div className="  bg-white -skew-x-12 [&>*]:skew-x-12 shadow-[5px_5px_0_0_rgba(254,48,253,0.5)]">
+          <button
+            className="px-3 py-3 text-[#81209b] uppercase text-sm hover:[&>img]:rotate-90"
+            onClick={() => handleRefresh()}
+          >
+            <img
+              src={refresh_icon}
+              alt="refresh"
+              className="w-6 transition-all"
+            />
+          </button>
+        </div>
 
-      <div className="  bg-white -skew-x-12 [&>*]:skew-x-12 shadow-[5px_5px_0_0_rgba(254,48,253,0.5)]">
-        <div className="px-3 py-3 text-[#81209b] uppercase text-sm hover:[&>div]:scale-90">
-          <AudioButton />
+        <div className="  bg-white -skew-x-12 [&>*]:skew-x-12 shadow-[5px_5px_0_0_rgba(254,48,253,0.5)]">
+          <div className="px-3 py-3 text-[#81209b] uppercase text-sm hover:[&>div]:scale-90">
+            <AudioButton />
+          </div>
         </div>
       </div>
-      </div>
 
-      <h1 className=" text-white mb-2 uppercase font-bold flex flex-col">
-        <span className="text-stroke-3 text-center text-2xl">{instance}</span>
-        <span className="bg-white text-center text-black px-2">
-          champions 2023-2024
-        </span>
-      </h1>
+      <motion.div
+        key={instance}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <h1 className=" text-white mb-2 uppercase font-bold flex flex-col">
+          <span className="text-stroke-3 text-center text-2xl">{instance}</span>
+          <span className="bg-white text-center text-black px-2">
+            champions 2023-2024
+          </span>
+        </h1>
+      </motion.div>
       <div
         className={`flex justify-center mt-8 ${
           instance === Instances.FINAL ? "" : "gap-[32rem] max-lg:gap-6"
@@ -56,14 +71,22 @@ function App() {
         <div className="flex flex-col gap-6">
           {[...matchups.slice(...matchkeys(instance)[0])].map(
             ({ teamA, teamB, scoreA, scoreB }: Matchup, index: number) => (
-              <MatchBox
-                isFinal={instance === Instances.FINAL}
-                key={index}
-                teamA={teamA}
-                teamB={teamB}
-                scoreA={scoreA}
-                scoreB={scoreB}
-              />
+              <motion.div
+                key={teamA.name}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <MatchBox
+                  isFinal={instance === Instances.FINAL}
+                  key={index}
+                  teamA={teamA}
+                  teamB={teamB}
+                  scoreA={scoreA}
+                  scoreB={scoreB}
+                />
+              </motion.div>
             )
           )}
         </div>
@@ -107,13 +130,21 @@ function App() {
         <div className="flex flex-col gap-6">
           {[...matchups.slice(...matchkeys(instance)[1])].map(
             ({ teamA, teamB, scoreA, scoreB }: Matchup, index: number) => (
-              <MatchBox
-                key={index}
-                teamA={teamA}
-                teamB={teamB}
-                scoreA={scoreA}
-                scoreB={scoreB}
-              />
+              <motion.div
+                key={teamA.name}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                exit={{ opacity: 0 }}
+                transition={{ duration: 1 }}
+              >
+                <MatchBox
+                  key={index}
+                  teamA={teamA}
+                  teamB={teamB}
+                  scoreA={scoreA}
+                  scoreB={scoreB}
+                />
+              </motion.div>
             )
           )}
         </div>
