d634ff1cfd5435498fad07c4392f0ab733ce18f5
Samuel Figueroa <samuelfs96@gmail.com>
2025-04-07 19:41:20 -0300

    feat: fix responsive on matchbox

--- src/components/MatchBox.tsx
+++ src/components/MatchBox.tsx
@@ -19,11 +19,11 @@ export default function MatchBox({
     <div
       className={`flex justify-between items-center ${
         isFinal ? "bg-[#81209b]" : ""
-      } border-[#81209b] border-[1px] max-md:w-[380px] max-md:gap-0 px-8 py-4 gap-12 -skew-x-12 [&>*]:skew-x-12`}
+      } border-[#81209b] border-[1px] max-md:w-full max-md:gap-8 px-8 max-md:px-4 py-4 gap-12 -skew-x-12 [&>*]:skew-x-12`}
     >
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center max-md:w-[80px]">
         <div className="flex items-center flex-col gap-1">
-          <img className="w-[62px] h-[62px]" src={teamA.logo} alt="teamlogo1" />
+          <img className="w-[62px] h-[62px] max-md:w-[40px] max-md:h-[40px]" src={teamA.logo} alt="teamlogo1" />
           <h1 className="text-center text-white uppercase text-xs font-semibold">
             {teamA.name}
           </h1>
@@ -44,9 +44,9 @@ export default function MatchBox({
         </div>
         {/* <p className="text-white text-sm font-bold absolute bottom-[-50%]">FT'</p> */}
       </div>
-      <div className="flex justify-center items-center">
+      <div className="flex justify-center items-center max-md:w-[80px]">
         <div className="flex items-center flex-col gap-1">
-          <img className="w-[62px] h-[62px]" src={teamB.logo} alt="teamlogo2" />
+          <img className="w-[62px] h-[62px] max-md:w-[40px] max-md:h-[40px]" src={teamB.logo} alt="teamlogo2" />
           <h1 className="text-center text-white uppercase text-xs font-semibold">
             {teamB.name}
           </h1>
